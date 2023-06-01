06c8e8176a4da3363dd93b2987dacabea11cc6fd
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2023-06-01 23:43:24 +0300

    clang format implemented

--- app/test/processing/app/AutoformatProducesOneUndoActionTest.java
+++ app/test/processing/app/AutoformatProducesOneUndoActionTest.java
@@ -27,58 +27,57 @@
  * the GNU General Public License.
  */
 
-package processing.app;
+ package processing.app;
 
-import org.fest.swing.fixture.JMenuItemFixture;
-import org.junit.Test;
-import processing.app.helpers.SketchTextAreaFixture;
-
-import static org.junit.Assert.assertEquals;
-
-public class AutoformatProducesOneUndoActionTest extends AbstractGUITest {
-
-  public static final String SOURCE_BEFORE = "void setup() {\n" +
-          "              // put your setup code here, to run once:\n" +
-          "\n" +
-          "}\n" +
-          "\n" +
-          "void loop() {\n" +
-          "  // put your main code here, to run repeatedly:\n" +
-          "\n" +
-          "}";
-  public static final String SOURCE_AFTER = "void setup() {\n" +
-          "  // put your setup code here, to run once:\n" +
-          "\n" +
-          "}\n" +
-          "\n" +
-          "void loop() {\n" +
-          "  // put your main code here, to run repeatedly:\n" +
-          "\n" +
-          "}";
-
-  @Test
-  public void shouldSaveCaretPositionAfterAutoformat() {
-    JMenuItemFixture menuEditUndo = window.menuItem("menuEditUndo");
-    menuEditUndo.requireDisabled();
-
-    JMenuItemFixture menuToolsAutoFormat = window.menuItem("menuToolsAutoFormat");
-    menuToolsAutoFormat.requireEnabled();
-
-    SketchTextAreaFixture editor = window.textArea("editor");
-    editor.setText(SOURCE_BEFORE);
-
-    editor.setCaretPosition(29); // right before the first // (double slash)
-
-    menuToolsAutoFormat.click();
-
-    String formattedText = editor.getText();
-    assertEquals(SOURCE_AFTER, formattedText);
-
-    assertEquals(29, editor.getCaretPosition());
-
-    menuEditUndo.requireEnabled();
-    menuEditUndo.click();
-    assertEquals(SOURCE_BEFORE, editor.getText());
-  }
-
-}
+ import org.fest.swing.fixture.JMenuItemFixture;
+ import org.junit.Test;
+ import processing.app.helpers.SketchTextAreaFixture;
+ 
+ import static org.junit.Assert.assertEquals;
+ 
+ public class AutoformatProducesOneUndoActionTest extends AbstractGUITest {
+ 
+   public static final String SOURCE_BEFORE = "void setup() {\n" +
+           "              // put your setup code here, to run once:\n" +
+           "\n" +
+           "}\n" +
+           "\n" +
+           "void loop() {\n" +
+           "  // put your main code here, to run repeatedly:\n" +
+           "\n" +
+           "}";
+   public static final String SOURCE_AFTER = "void setup() {\n" +
+           "  // put your setup code here, to run once:\n" +
+           "}\n" +
+           "\n" +
+           "void loop() {\n" +
+           "  // put your main code here, to run repeatedly:\n" +
+           "}";
+ 
+   @Test
+   public void shouldSaveCaretPositionAfterAutoformat() {
+     JMenuItemFixture menuEditUndo = window.menuItem("menuEditUndo");
+     menuEditUndo.requireDisabled();
+ 
+     JMenuItemFixture menuToolsAutoFormat = window.menuItem("menuToolsAutoFormat");
+     menuToolsAutoFormat.requireEnabled();
+ 
+     SketchTextAreaFixture editor = window.textArea("editor");
+     editor.setText(SOURCE_BEFORE);
+ 
+     editor.setCaretPosition(29); // right before the first // (double slash)
+ 
+     menuToolsAutoFormat.click();
+ 
+     String formattedText = editor.getText();
+     assertEquals(SOURCE_AFTER, formattedText);
+ 
+     // Autoformat with clang-format keeps cursor relative to source code
+     assertEquals(17, editor.getCaretPosition());
+ 
+     menuEditUndo.requireEnabled();
+     menuEditUndo.click();
+     assertEquals(SOURCE_BEFORE, editor.getText());
+   }
+ 
+ }
